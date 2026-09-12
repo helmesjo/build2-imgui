33c4c12081607f48993831f07d79b6a83287b860
helmesjo <helmesjo@gmail.com>
2026-09-12 04:13:52 +0200

    Write smoke test for libimgui

--- libimgui/tests/basics/driver.cpp
+++ libimgui/tests/basics/driver.cpp
@@ -1,34 +1,39 @@
-#include <sstream>
-#include <stdexcept>
-
 #include <imgui.h>
 
 #undef NDEBUG
 #include <cassert>
 
+// Smoke test for the core library. Does not require any platform or
+// renderer backend: exercises context creation, a frame, and a widget
+// call, then checks that draw data was produced.
+
 int main ()
 {
-  using namespace std;
-  using namespace imgui;
+  assert (ImGui::GetVersion () != nullptr);
 
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
+  ImGuiContext* ctx (ImGui::CreateContext ());
+  assert (ctx != nullptr);
 
-  // Empty name.
+  ImGuiIO& io (ImGui::GetIO ());
+  io.IniFilename = nullptr; // Don't read/write imgui.ini for this test.
+  io.DisplaySize = ImVec2 (1920.0f, 1080.0f);
+  io.DeltaTime = 1.0f / 60.0f;
+
+  // Build the default font atlas ourselves since there is no renderer
+  // backend to do it for us (that's normally its job).
   //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+  unsigned char* pixels;
+  int width, height;
+  io.Fonts->GetTexDataAsRGBA32 (&pixels, &width, &height);
+  assert (pixels != nullptr);
+
+  ImGui::NewFrame ();
+  ImGui::Begin ("Test");
+  ImGui::Text ("Hello, Dear ImGui!");
+  ImGui::End ();
+  ImGui::Render ();
+
+  assert (ImGui::GetDrawData () != nullptr);
+
+  ImGui::DestroyContext (ctx);
 }
