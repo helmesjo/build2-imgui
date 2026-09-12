4ec630334ffd54008994268b5218cee8c8a4269b
helmesjo <helmesjo@gmail.com>
2026-09-12 04:12:13 +0200

    Fill libimgui/ with upstream source symlinks

--- libimgui/src/imgui.cpp
+++ libimgui/src/imgui.cpp
@@ -1,17 +1,1 @@
-#include <imgui.h>
-
-#include <ostream>
-#include <stdexcept>
-
-using namespace std;
-
-namespace imgui
-{
-  void say_hello (ostream& o, const string& n)
-  {
-    if (n.empty ())
-      throw invalid_argument ("empty name");
-
-    o << "Hello, " << n << '!' << endl;
-  }
-}
+../../upstream/imgui.cpp
